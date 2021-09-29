e58fb50637845711de703a87ad225a43a73b38ad
hkursatsimsek <developer.hkursatsimsek@gmail.com>
2021-09-29 09:48:28 +0300

    First commit

---
+++ swiftUI-exercises/ContentView.swift
@@ -0,0 +1,42 @@
+//
+//  ContentView.swift
+//  swiftUI-exercises
+//
+//  Created by Kürşat Şimşek on 28.09.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+
+    @State var result: Float = 0
+    @State var number: String = ""
+    
+    var body: some View {
+        VStack{
+            TextField("Sayı Giriniz", text:$number)
+                .padding()
+                .multilineTextAlignment(.center)
+                .foregroundColor(.gray)
+                .border(Color.gray, width: 2)
+                .keyboardType(.numberPad)
+            
+            Button("1.18 Katını Hesapla") {
+                result = (number as NSString).floatValue * 1.18
+            }.padding()
+            .border(Color.blue, width: 2)
+            .cornerRadius(5)
+            
+            Text(String(result))
+                .padding()
+                .foregroundColor(.red)
+            
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ swiftUI-exercises/swiftUI_exercisesApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftUI_exercisesApp.swift
+//  swiftUI-exercises
+//
+//  Created by Kürşat Şimşek on 28.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct swiftUI_exercisesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
